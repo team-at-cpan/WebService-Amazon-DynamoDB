9db8a1ead43be1568d6748bdb6f338ba51e271b7
Tom Molesworth <tom@perlsite.co.uk>
2015-01-25 09:19:05 +0000

    Item lookup for table

--- lib/WebService/Amazon/DynamoDB/Server/Table.pm
+++ lib/WebService/Amazon/DynamoDB/Server/Table.pm
@@ -3,9 +3,23 @@ package WebService::Amazon::DynamoDB::Server::Table;
 use strict;
 use warnings;
 
+use Future;
+
 sub new { my $class = shift; bless {@_}, $class }
 
-sub name { shift->{TableName} }
+sub name { shift->{TableName} // die 'invalid table - no name' }
+
+sub item_by_id {
+	my ($self, @id) = @_;
+	my $k = $self->key_for_id(@id) // return Future->fail('bad key');
+	exists $self->{items}{$k} or return Future->fail('item not found');
+	Future->done($self->{items}{$k});
+}
+
+sub key_for_id {
+	my ($self, @id) = @_;
+	join "\0", map Encode::encode("UTF-8", $_), @id;
+}
 
 1;
 
