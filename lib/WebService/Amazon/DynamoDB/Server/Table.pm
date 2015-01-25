10cd462e8ed9c22d4ba19189623774300803ebd6
Tom Molesworth <tom@perlsite.co.uk>
2015-01-25 10:07:05 +0000

    Fill out some more docs

--- lib/WebService/Amazon/DynamoDB/Server/Table.pm
+++ lib/WebService/Amazon/DynamoDB/Server/Table.pm
@@ -51,6 +51,10 @@ sub key_for_id {
 	join "\0", map Encode::encode("UTF-8", $_), @id;
 }
 
+=head2 bytes_used
+
+=cut
+
 sub bytes_used {
 	my ($self) = @_;
 	$self->{bytes_used} //= do {
@@ -64,6 +68,10 @@ sub bytes_used {
 	}
 }
 
+=head2 validate_id_for_item_data
+
+=cut
+
 sub validate_id_for_item_data {
 	my ($self, $data) = @_;
 	my @id_fields = map $_->{AttributeName}, @{$self->{KeySchema}};
@@ -74,5 +82,40 @@ sub validate_id_for_item_data {
 	my ($id) = join "\0", map values %{$data->{$_}}, @id_fields;
 	Future->done($id);
 }
+
 1;
+
+__END__
+
+=head1 EVENTS
+
+The following events may be raised on the message bus used by this
+class - use L<Mixin::Event::Dispatch/subscribe_to_event> to watch
+for them:
+
+ $srv->bus->subscribe_to_event(
+   list_tables => sub {
+     my ($ev, $tables, $req, $resp) = @_;
+	 ...
+   }
+ );
+
+Note that most of these include a L<Future> - the event is triggered
+once the L<Future> is marked as ready, so it should be safe to call
+C< get > to examine the current state:
+
+ $srv->bus->subscribe_to_event(
+   create_table => sub {
+     my ($ev, $tbl, $req, $resp) = @_;
+	 warn "Had a failed table creation request" unless eval { $resp->get };
+   }
+ );
+
+=head1 AUTHOR
+
+Tom Molesworth <cpan@perlsite.co.uk>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2013-2015. Licensed under the same terms as Perl itself.
 
